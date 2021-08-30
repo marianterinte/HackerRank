00fa32f9d8c55614ee588033a99460412d8118b9
Marian Terinte <marian.terinte@gmail.com>
2021-08-30 12:53:00 +0300

    counting valleys

---
+++ CountingValeys/CountingValyes/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+	class Program
+	{
+		public static int countingValleys(int steps, string path)
+		{
+			int seaLevel = 0;
+			char last = path[0];
+			int valeys = 0;
+
+			for (int i = 0; i < steps; i++)
+			{
+				if (seaLevel == 0 && path[i] == 'D')
+					valeys++;
+
+				if (path[i] == 'U')
+					seaLevel++;
+				else
+					seaLevel--;
+			}
+			return valeys;
+		}
+
+		static void Main(string[] args)
+		{
+			string path = "DDUUDDUDUUUD";
+			int n = 12;
+			var res = countingValleys(n, path);
+
+			Console.WriteLine("res=" + res);
+		}
+	}
+}
