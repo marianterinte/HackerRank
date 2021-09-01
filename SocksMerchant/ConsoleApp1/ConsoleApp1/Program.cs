d5298a6de9006e3f6b3837bfb9a8ebf00a4990a0
Marian Terinte <marian.terinte@gmail.com>
2021-09-01 12:10:25 +0300

    socksmerchant

---
+++ SocksMerchant/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			int n = 9;
+			List<int> ar = new List<int> { 10, 20, 20, 10, 10, 30, 50, 10, 20 };
+			var result = sockMerchant(n, ar);
+			Console.WriteLine("nr socks=" + result);
+
+		}
+
+		public static int sockMerchant(int n, List<int> ar)
+		{
+			Dictionary<int, int> resAr = ar.Distinct().ToDictionary(c => c, c => 0);
+
+			for (int i = 0; i < n; i++)
+			{
+				if (resAr.ContainsKey(ar[i]))
+					resAr[ar[i]] = resAr[ar[i]] + 1;
+			}
+
+			int result = resAr.Sum(s => s.Value/2);
+			return result;
+		}
+	}
+}
